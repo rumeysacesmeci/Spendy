8fe594b58ea75c191cdae289ab73328de94bb527
rmys6 <37138243+rmys6@users.noreply.github.com>
2020-11-25 18:54:02 +0300

    New activities created.

---
+++ app/src/main/java/com/example/spendy/Statistics.kt
@@ -0,0 +1,11 @@
+package com.example.spendy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Statistics : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistics)
+    }
+}
